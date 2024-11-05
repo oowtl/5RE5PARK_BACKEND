c3de0886ea9a111b4cfae7edc230454f857cdc1a
oowtl <oowtl7425@gmail.com>
2024-11-05 23:15:35 +0900

    [chore] : 의존성 추가 및 폴더 셋팅

---
+++ src/main/java/com/oreo/finalproject_5re5_be/FinalProject5Re5BeApplication.java
@@ -0,0 +1,13 @@
+package com.oreo.finalproject_5re5_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProject5Re5BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProject5Re5BeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oreo/finalproject_5re5_be/FinalProject5Re5BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oreo.finalproject_5re5_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProject5Re5BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
