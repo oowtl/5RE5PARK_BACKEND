930bbc278cf1b69ec04f82f90058f0b7516895a7
jongheonleee <qwefghnm1212@gmail.com>
2024-11-07 17:06:38 +0900

    [chore] 프로젝트 엔티티 정의
    프로젝트 엔티티 정의. 총 2개 엔티티로 구성되어 있습니다.

---
+++ src/main/java/com/oreo/finalproject_5re5_be/project/entity/ProjectStatus.java
@@ -0,0 +1,38 @@
+package com.oreo.finalproject_5re5_be.project.entity;
+
+
+import com.oreo.finalproject_5re5_be.utils.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "project_status")
+@Getter @Setter
+@ToString
+public class ProjectStatus extends BaseEntity {
+
+    @Id
+    @Column(name = "ps_seq")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long psSeq;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "pro_seq")
+    private Project project;
+
+    /** 추후에 통합 코드 엔티티와 연관관계 맺기 **/
+    private Long ccSeq;
+
+    private LocalDateTime applDate;
+    private LocalDateTime endDate;
+}
