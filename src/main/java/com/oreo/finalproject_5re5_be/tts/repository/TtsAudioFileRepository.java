33804eb74e2bdfbc9874f147040a2ffbe661012b
oowtl <oowtl7425@gmail.com>
2024-11-08 23:09:26 +0900

    [chore] : Repository 어노테이션 추가

--- src/main/java/com/oreo/finalproject_5re5_be/tts/repository/TtsAudioFileRepository.java
+++ src/main/java/com/oreo/finalproject_5re5_be/tts/repository/TtsAudioFileRepository.java
@@ -2,6 +2,8 @@ package com.oreo.finalproject_5re5_be.tts.repository;
 
 import com.oreo.finalproject_5re5_be.tts.entity.TtsAudioFile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TtsAudioFileRepository extends JpaRepository<TtsAudioFile, Long> {
 }
