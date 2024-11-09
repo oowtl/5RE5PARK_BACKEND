109ba21176e9a7eaade8c2b54248c0d298f7523e
alswooy <jjang990312@naver.com>
2024-11-09 13:42:20 +0900

    [ci] CI 빌드 테스트 수정 및 test 환경 h2로 변경 - ci if문 삭제 - 테스트 h2환경으로 변경

--- src/test/java/com/oreo/finalproject_5re5_be/FinalProject5Re5BeApplicationTests.java
+++ src/test/java/com/oreo/finalproject_5re5_be/FinalProject5Re5BeApplicationTests.java
@@ -2,8 +2,10 @@ package com.oreo.finalproject_5re5_be;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class FinalProject5Re5BeApplicationTests {
 
     @Test
