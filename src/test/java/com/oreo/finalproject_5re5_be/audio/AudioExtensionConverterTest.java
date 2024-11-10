ebe6b101383e0230bad1c8fe8ba90c087857411e
K-KY <kyuyoungk@naver.com>
2024-11-10 18:08:10 +0900

    [Test] : 오디오 확장자 확인 테스트 변경
    - `AudioExtensionChecker` 을 사용하도록 변경

--- src/test/java/com/oreo/finalproject_5re5_be/audio/AudioExtensionConverterTest.java
+++ src/test/java/com/oreo/finalproject_5re5_be/audio/AudioExtensionConverterTest.java
@@ -1,15 +1,15 @@
 package com.oreo.finalproject_5re5_be.audio;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-import javax.sound.sampled.AudioFileFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.File;
 import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest
@@ -17,9 +17,22 @@ import java.io.IOException;
 class AudioExtensionConverterTest {
 
     @Test
+    @DisplayName("wav 파일 변환 테스트")
     void convertMp3ToWavTest() throws UnsupportedAudioFileException, IOException {
-//        AudioInputStream audioInputStream = AudioExtensionConverter.mp3ToWav(new File("/Users/kyuyoung/Downloads/lora.mp3"));
+        byte[] audioInputStream = AudioExtensionConverter.mp3ToWav(new File("test.mp3"));
+        boolean wavExtension = AudioExtensionChecker.isWavExtension(audioInputStream);
 
-//        AudioSystem.write(audioInputStream, AudioFileFormat.Type.WAVE, new File("test.wav"));
+        assertThat(wavExtension).isTrue();
     }
+
+    @Test
+    @DisplayName("이미 wav형식 파일인 경우 테스트")
+    void convertWavToWavTest() throws UnsupportedAudioFileException, IOException {
+        byte[] audioInputStream = AudioExtensionConverter.mp3ToWav(new File("test.wav"));
+        boolean wavExtension = AudioExtensionChecker.isWavExtension(audioInputStream);
+
+        assertThat(wavExtension).isTrue();
+    }
+
+
 }
