11cb6c15a9efa877a083338582eb1786906509e8
oowtl <oowtl7425@gmail.com>
2024-11-10 15:22:08 +0900

    [feat] VoiceRepository 추가 테스트용 find 메서드 추가

---
+++ src/main/java/com/oreo/finalproject_5re5_be/tts/repository/VoiceRepository.java
@@ -0,0 +1,17 @@
+package com.oreo.finalproject_5re5_be.tts.repository;
+
+import com.oreo.finalproject_5re5_be.tts.entity.Voice;
+import org.springframework.data.domain.Example;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.FluentQuery;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+@Repository
+public interface VoiceRepository extends JpaRepository<Voice, Long> {
+    List<Voice> findAllByName(String name);
+    List<Voice> findAllByNameContaining(String name);
+}
