9b2bbc6e3a0673ee090a8198e26a6bf792c734cb
oowtl <oowtl7425@gmail.com>
2024-11-10 15:21:44 +0900

    [feat] Voice Entity 수정 Lombok 애너테이션 추가 및 삭제

--- src/main/java/com/oreo/finalproject_5re5_be/tts/entity/Voice.java
+++ src/main/java/com/oreo/finalproject_5re5_be/tts/entity/Voice.java
@@ -2,13 +2,18 @@ package com.oreo.finalproject_5re5_be.tts.entity;
 
 import com.oreo.finalproject_5re5_be.global.entity.BaseEntity;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "voice")
 @Getter
-@Setter
+@ToString
+// builder 패턴을 사용할 수 있게 해주는 어노테이션
+@Builder(toBuilder = true) // toBuilder = true : 객체의 일부 값을 변경하여 생성시키고 싶을 때 사용한다.
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 파라미터가 없는 생성자를 만들어주는 어노테이션, PROTECTED 접근 제어자
+@AllArgsConstructor(access = AccessLevel.PRIVATE) // 전체 파라미터를 가지는 생성자를 만들어주는 어노테이션, PRIVATE 접근 제어자
+// equals()와 hashCode() 메소드를 자동으로 생성해주는 어노테이션
+@EqualsAndHashCode(callSuper = false) // callSuper = false : 부모 클래스의 필드를 비교하지 않는다.
 public class Voice extends BaseEntity {
     @Id
     @Column(name = "voice_seq")
