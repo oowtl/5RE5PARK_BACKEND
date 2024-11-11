343e2366b1464e7d90fb5dcc6a1a51d515d461e1
alswooy <108874185+alswooy@users.noreply.github.com>
2024-11-11 16:30:12 +0900

    [feature] Vc Service 추가 - repository 추가 - dto 추가 - service 추가

---
+++ src/main/java/com/oreo/finalproject_5re5_be/vc/repository/VcTextRepository.java
@@ -0,0 +1,9 @@
+package com.oreo.finalproject_5re5_be.vc.repository;
+
+import com.oreo.finalproject_5re5_be.vc.entity.VcText;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VcTextRepository extends JpaRepository<VcText, Long> {
+}
