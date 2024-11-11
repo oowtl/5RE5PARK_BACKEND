0162c85712b66f8c6a504fb1263eb6bd32087d99
jongheonleee <qwefghnm1212@gmail.com>
2024-11-11 21:54:51 +0900

    [comment] 주석 정리

--- src/test/java/com/oreo/finalproject_5re5_be/member/service/MemberServiceImplTestByMock.java
+++ src/test/java/com/oreo/finalproject_5re5_be/member/service/MemberServiceImplTestByMock.java
@@ -57,44 +57,64 @@ class MemberServiceImplTestByMock {
     @DisplayName("회원가입 - 중복된 이메일")
     @Test
     public void 중복된_이메일_예외_발생() {
+        // 회원 약관 정보 생성
         List<MemberTermRequest> memberTermRequests = retryableCreateMemberMemberTerms();
+        // 회원 입력 데이터 생성
         MemberRegisterRequest request = retryableCreateMemberMemberRegisterRequest(memberTermRequests);
+        // 회원 엔티티 생성
         Member member = request.createMemberEntity();
+        // 이메일 중복시 해당 회원 엔티티 반환하게 세팅
         when(memberRepository.findByEmail(request.getEmail())).thenReturn(member);
+        // 실행 및 예외 발생 여부 파악
         assertThrows(MemberDuplicatedEmailException.class, () -> memberService.create(request));
     }
 
     @DisplayName("회원가입 - 중복된 아이디")
     @Test
     public void 중복된_아이디_예외_발생() {
+        // 회원 약관 정보 생성
         List<MemberTermRequest> memberTermRequests = retryableCreateMemberMemberTerms();
+        // 회원 입력 데이터 생성
         MemberRegisterRequest request = retryableCreateMemberMemberRegisterRequest(memberTermRequests);
+        // 회원 엔티티 생성
         Member member = request.createMemberEntity();
+        // 아이디 중복시 해당 회원 엔티티 반환하게 세팅
         when(memberRepository.findById(request.getId())).thenReturn(member);
+        // 실행 및 예외 발생 여부 파악
         assertThrows(MemberDuplicatedIdException.class, () -> memberService.create(request));
     }
 
     @DisplayName("회원가입 - 필수 약관 미동의")
     @Test
     public void 필수_약관_미동의_예외_발생() {
+        // 회원 약관 정보 생성
         List<MemberTermRequest> memberTermRequests = retryableCreateMemberMemberTerms();
+        // 필수 약관 미동의로 변경
         memberTermRequests.get(0).setAgreed('N');
+        // 회원 입력 데이터 생성
         MemberRegisterRequest request = retryableCreateMemberMemberRegisterRequest(memberTermRequests);
+        // 회원 엔티티 생성
         Member member = request.createMemberEntity();
+        // 실행 및 예외 발생 여부 파악
         assertThrows(MemberMandatoryTermNotAgreedException.class, () -> memberService.create(request));
     }
 
     @DisplayName("회원가입 - 5개의 약관 항목 보다 많은 경우")
     @Test
     public void 약관_항목_초과_예외_발생() {
+        // 회원 약관 정보 생성
         List<MemberTermRequest> memberTermRequests = retryableCreateMemberMemberTerms();
+        // 회원 약관 추가 6개로 세팅
         memberTermRequests.add(MemberTermRequest.builder()
                         .termCondCode(6L)
                         .agreed('Y')
                         .isMandatory(true)
                         .build());
+        // 회원 입력 데이터 생성
         MemberRegisterRequest request = retryableCreateMemberMemberRegisterRequest(memberTermRequests);
+        // 회원 엔티티 생성
         Member member = request.createMemberEntity();
+        // 실행 및 예외 발생 여부 파악
         assertThrows(MemberWrongCountTermCondition.class, () -> memberService.create(request));
     }
 
@@ -108,11 +128,18 @@ class MemberServiceImplTestByMock {
     @DisplayName("스프링시큐리티에서 호출하는 loadUserByUsername() 성공 테스트 ")
     @Test
     public void 스프링시큐리티_회원_조회_성공() {
+        // 회원 약관 정보 생성
         List<MemberTermRequest> memberTermRequests = retryableCreateMemberMemberTerms();
+        // 회원 입력 데이터 생성
         MemberRegisterRequest memberRegisterRequest = retryableCreateMemberMemberRegisterRequest(
                 memberTermRequests);
+        // 입력 데이터로부터 회원 엔티티 생성
         Member foundMember = memberRegisterRequest.createMemberEntity();
+
+        // "qwerfde2312" 조회시 위에 생성한 엔티티 반환하게 세팅
         when(memberRepository.findById("qwerfde2312")).thenReturn(foundMember);
+
+        // 실행 및 결과 비교
         UserDetails foundMemberDetails = memberService.loadUserByUsername("qwerfde2312");
         assertEquals(foundMemberDetails.getUsername(), foundMember.getId());
     }
@@ -120,11 +147,16 @@ class MemberServiceImplTestByMock {
     @DisplayName("스프링시큐리티에서 호출하는 loadUserByUsername() 실패 테스트 ")
     @Test
     public void 스프링시큐리티_회원_조회_실패() {
+        // 회원 약관 정보 생성
         List<MemberTermRequest> memberTermRequests = retryableCreateMemberMemberTerms();
+        // 입력 데이터로부터 회원 엔티티 생성
         MemberRegisterRequest memberRegisterRequest = retryableCreateMemberMemberRegisterRequest(
                 memberTermRequests);
+        // 입력 데이터로부터 회원 엔티티 생성
         Member foundMember = memberRegisterRequest.createMemberEntity();
+        // "qwerfde2312" 조회시 null 반환하게 세팅
         when(memberRepository.findById("qwerfde2312")).thenReturn(null);
+        // 실행 및 예외 발생 여부 파악
         assertThrows(UsernameNotFoundException.class,
                 () -> memberService.loadUserByUsername("qwerfde2312"));
     }
@@ -214,7 +246,7 @@ class MemberServiceImplTestByMock {
 
     // 추후에 해당 부분 문제 해결 : 회원 상태 어떻게 비교할지 고민하기
     private boolean isSameMemberStateFields(MemberState memberState) {
-        // 회원 상태 비교
+        // 회원 상태 비교(아직 데이터 세팅해 놓지 않은 상태임)
         return true;
     }
 }
