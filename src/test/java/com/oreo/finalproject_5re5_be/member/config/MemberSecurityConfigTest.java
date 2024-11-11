f3fa5b1d8b62217a0b964aa5ca6b8eccd0077293
jongheonleee <qwefghnm1212@gmail.com>
2024-11-11 18:42:46 +0900

    [test] 스프링 시큐리티 로그인/로그아웃 처리 동작 테스트 완료(다른 파트 개발 어느정도 완료되면 인증/인가 처리 구현 및 테스트 진행 예정)

--- src/test/java/com/oreo/finalproject_5re5_be/member/config/MemberSecurityConfigTest.java
+++ src/test/java/com/oreo/finalproject_5re5_be/member/config/MemberSecurityConfigTest.java
@@ -1,12 +1,20 @@
 package com.oreo.finalproject_5re5_be.member.config;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.oreo.finalproject_5re5_be.member.dto.request.MemberRegisterRequest;
+import com.oreo.finalproject_5re5_be.member.dto.request.MemberTermRequest;
+import com.oreo.finalproject_5re5_be.member.entity.Member;
+import com.oreo.finalproject_5re5_be.member.repository.MemberRepository;
 import com.oreo.finalproject_5re5_be.member.service.MemberServiceImpl;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,6 +25,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -30,8 +42,13 @@ class MemberSecurityConfigTest {
     @Autowired
     private LoginAuthenticationSuccessHandler successHandler;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @MockBean
     private MemberServiceImpl memberService;
+    @MockBean
+    private MemberRepository memberRepository;
 
 
     @BeforeEach
@@ -43,18 +60,61 @@ class MemberSecurityConfigTest {
     @Test
     @DisplayName("로그인 성공 테스트")
     void 로그인_성공() throws Exception {
-        // 로그인 정보를 입력 받음
+        // 회원 약관 동의 정보 생성
+        List<MemberTermRequest> memberTermRequests = retryableCreateMemberMemberTerms();
+        // 회원 가입 정보 생성
+        MemberRegisterRequest memberRegisterRequest = retryableCreateMemberMemberRegisterRequest(memberTermRequests);
+        // 가입 정보로 부터 회원 엔티티 생성
+        Member foundMember = memberRegisterRequest.createMemberEntity();
+        // 비밀번호 암호화 처리 후 저장
+        String encodedPassword = passwordEncoder.encode(foundMember.getPassword());
+        foundMember.setPassword(encodedPassword);
 
-        // 서비스에서 로그인 성공되게 설정
-        // 로그인 성공 후 리다이렉션 확인
+        // 반환할 UserDetails 객체 생성
+        UserDetails foundMemberDetails = User.withUsername(foundMember.getId())
+                                             .password(encodedPassword)
+                                             .build();
+
+        // 목객체 동작 지정
+        when(memberRepository.findById(foundMember.getId())).thenReturn(foundMember); // 회원 조회시 가입 정보로 부터 만든 회원 엔티티 반환
+        when(memberService.loadUserByUsername(foundMember.getId())).thenReturn(foundMemberDetails); // 회원 아이디(이름)으로 로드시 이전에 만들었던 UserDetails 반환
+
+        // 로그인 요청
+        mockMvc.perform(formLogin("/api/member/login")
+                        .user(memberRegisterRequest.getId())
+                        .password(memberRegisterRequest.getPassword()))  // 실제 인코딩 전 비밀번호 전달
+                .andExpect(status().isOk());
+
     }
 
     @Test
-    @DisplayName("로그인 실패 테스트")
+    @DisplayName("로그인 실패 테스트, 로그인 실패시 로그인 페이지로 리디렉션")
     void 로그인_실패() throws Exception {
-        // 로그인 정보를 입력 받음
-        // 로그인 실패로 예외 반환 확인
+        // 회원 약관 동의 정보 생성
+        List<MemberTermRequest> memberTermRequests = retryableCreateMemberMemberTerms();
+        // 회원 가입 정보 생성
+        MemberRegisterRequest memberRegisterRequest = retryableCreateMemberMemberRegisterRequest(memberTermRequests);
+        // 가입 정보로 부터 회원 엔티티 생성
+        Member foundMember = memberRegisterRequest.createMemberEntity();
+        // 비밀번호 암호화 처리 후 저장
+        String encodedPassword = passwordEncoder.encode(foundMember.getPassword());
+        foundMember.setPassword(encodedPassword);
 
+        // 반환할 UserDetails 객체 생성
+        UserDetails foundMemberDetails = User.withUsername(foundMember.getId())
+                .password(encodedPassword)
+                .build();
+
+        // 목객체 동작 지정
+        when(memberRepository.findById(foundMember.getId())).thenReturn(foundMember); // 회원 조회시 가입 정보로 부터 만든 회원 엔티티 반환
+        when(memberService.loadUserByUsername(foundMember.getId())).thenReturn(foundMemberDetails); // 회원 아이디(이름)으로 로드시 이전에 만들었던 UserDetails 반환
+
+        // 로그인 요청
+        mockMvc.perform(formLogin("/api/member/login")
+                        .user(memberRegisterRequest.getId())
+                        .password("wrong password"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/api/member/login")); // 이 부분 추후에 프론트랑 얘기하기
     }
 
     // 인증되지 않은 비회원이 보호된 페이지에 접근하면 401 Unauthorized 응답 반환된다.
@@ -80,12 +140,6 @@ class MemberSecurityConfigTest {
         // 프론트엔드 로그인 페이지 요청 경로 정해지면 테스트하기
     }
 
-    // 로그인 실패시 로그인 페이지로 리디렉션
-    @Test
-    @DisplayName("로그인 실패 시 로그인 페이지로 리디렉션.")
-    void 로그인_실패_로그인_페이지_리다이렉션() throws Exception {
-        // 프론트엔드 로그인 페이지 요청 경로 정해지면 테스트하기
-    }
 
     // 로그아웃할 경우, 세션 무효화
     @Test
@@ -94,5 +148,67 @@ class MemberSecurityConfigTest {
         mockMvc.perform(get("/api/member/logout"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/"));
+    }
+
+    private MemberRegisterRequest retryableCreateMemberMemberRegisterRequest(
+            List<MemberTermRequest> memberTermRequests) {
+        var request = MemberRegisterRequest.builder()
+                .id("qwerfde2312")
+                .password("asdf12341234@")
+                .email("asdf3214@gmail.com")
+                .name("홍길동")
+                .birthDate("1990-01-01")
+                .userRegDate(LocalDateTime.now())
+                .chkValid('Y')
+                .memberTermRequests(memberTermRequests)
+                .normAddr("서울시 강남구")
+                .passAddr("서초대로 59-32")
+                .locaAddr("서초동")
+                .detailAddr("서초동 123-456")
+                .build();
+
+        return request;
+    }
+
+    private List<MemberTermRequest> retryableCreateMemberMemberTerms() {
+        List<MemberTermRequest> memberTermRequests = new ArrayList<>();
+        // 약관 동의 내용 설정
+        memberTermRequests = new ArrayList<>();
+        memberTermRequests.add(
+                MemberTermRequest.builder()
+                        .termCondCode(1L)
+                        .agreed('Y')
+                        .isMandatory(true)
+                        .build());
+
+        memberTermRequests.add(
+                MemberTermRequest.builder()
+                        .termCondCode(2L)
+                        .agreed('Y')
+                        .isMandatory(true)
+                        .build());
+
+        memberTermRequests.add(
+                MemberTermRequest.builder()
+                        .termCondCode(3L)
+                        .agreed('Y')
+                        .isMandatory(true)
+                        .build());
+
+        memberTermRequests.add(
+                MemberTermRequest.builder()
+                        .termCondCode(4L)
+                        .agreed('N')
+                        .isMandatory(false)
+                        .build());
+
+        memberTermRequests.add(
+                MemberTermRequest.builder()
+                        .termCondCode(5L)
+                        .agreed('N')
+                        .isMandatory(false)
+                        .build());
+
+        return memberTermRequests;
     }
 }
