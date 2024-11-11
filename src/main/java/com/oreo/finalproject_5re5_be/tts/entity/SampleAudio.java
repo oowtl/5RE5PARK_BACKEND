379a0a3e5af1c293f788f458570a9e498cf9d599
oowtl <oowtl7425@gmail.com>
2024-11-11 14:42:51 +0900

    [feat] : SampleAudio Entity 수정 lombok 애너테이션 추가 연관관계 매핑 추가

--- src/main/java/com/oreo/finalproject_5re5_be/tts/entity/SampleAudio.java
+++ src/main/java/com/oreo/finalproject_5re5_be/tts/entity/SampleAudio.java
@@ -2,14 +2,19 @@ package com.oreo.finalproject_5re5_be.tts.entity;
 
 import com.oreo.finalproject_5re5_be.global.entity.BaseEntity;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 
 @Entity
 @Table(name = "sample_audio")
 @Getter
-@Setter
+@ToString
+// builder 패턴을 사용할 수 있게 해주는 어노테이션
+@Builder(toBuilder = true) // toBuilder = true : 객체의 일부 값을 변경하여 생성시키고 싶을 때 사용한다.
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 파라미터가 없는 생성자를 만들어주는 어노테이션, PROTECTED 접근 제어자
+@AllArgsConstructor(access = AccessLevel.PRIVATE) // 전체 파라미터를 가지는 생성자를 만들어주는 어노테이션, PRIVATE 접근 제어자
+// equals()와 hashCode() 메소드를 자동으로 생성해주는 어노테이션
+@EqualsAndHashCode(callSuper = false) // callSuper = false : 부모 클래스의 필드를 비교하지 않는다.
 public class SampleAudio extends BaseEntity {
     @Id
     @Column(name = "smpl_aud_seq")
@@ -40,4 +45,8 @@ public class SampleAudio extends BaseEntity {
     @CreatedDate
     @Column(name = "created_at")
     private char createdAt;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "voice_seq")
+    private Voice voice;
 }
