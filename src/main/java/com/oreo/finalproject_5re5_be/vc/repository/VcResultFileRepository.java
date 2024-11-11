0957f58a21f5f38606aeb94e497a319de4924e02
alswooy <108874185+alswooy@users.noreply.github.com>
2024-11-11 20:45:06 +0900

    [feature] Vc Service 생성
    [Create]
    - SRC 파일 저장
    - TRG 파일 저장
    - Result 파일 저장
    - 텍스트 저장
    [Read]
    - 프로젝트 행 전체 조회
    - SRC 파일 다운로드, SRC 파일 재생
    - Result 파일 다운로드, Result 파일 재생
    [Update]
    - 행 텍스트 수정, 행 순서 변경

--- src/main/java/com/oreo/finalproject_5re5_be/vc/repository/VcResultFileRepository.java
+++ src/main/java/com/oreo/finalproject_5re5_be/vc/repository/VcResultFileRepository.java
@@ -4,6 +4,9 @@ import com.oreo.finalproject_5re5_be.vc.entity.VcResultFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VcResultFileRepository extends JpaRepository<VcResultFile, Long> {
+    VcResultFile findBySrcSeq(Long srcSeq);
 }
--- src/main/java/com/oreo/finalproject_5re5_be/vc/repository/VcSrcFileRepository.java
+++ src/main/java/com/oreo/finalproject_5re5_be/vc/repository/VcSrcFileRepository.java
@@ -4,6 +4,9 @@ import com.oreo.finalproject_5re5_be.vc.entity.VcSrcFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VcSrcFileRepository extends JpaRepository<VcSrcFile, Long> {
+    List<VcSrcFile> findByProjectId(Long projectId);
 }
