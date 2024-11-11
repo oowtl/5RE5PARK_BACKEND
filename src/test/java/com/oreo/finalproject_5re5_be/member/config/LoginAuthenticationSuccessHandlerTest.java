3a26b0806f0b437690605154df270ab04ea6f67f
jongheonleee <qwefghnm1212@gmail.com>
2024-11-11 18:45:52 +0900

    [chore] 사용하지 않는 임포트문, 주석 제거

--- src/test/java/com/oreo/finalproject_5re5_be/member/config/LoginAuthenticationSuccessHandlerTest.java
+++ src/test/java/com/oreo/finalproject_5re5_be/member/config/LoginAuthenticationSuccessHandlerTest.java
@@ -15,8 +15,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.security.core.Authentication;
