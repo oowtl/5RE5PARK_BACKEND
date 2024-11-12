7f01b04d03b894ee66f53ec6b8c2b4345d848a6b
K-KY <kyuyoungk@naver.com>
2024-11-12 15:13:09 +0900

    [Comment] : 주석 추가

--- src/test/java/com/oreo/finalproject_5re5_be/audio/ConcatenatorTest.java
+++ src/test/java/com/oreo/finalproject_5re5_be/audio/ConcatenatorTest.java
@@ -21,14 +21,14 @@ class ConcatenatorTest {
     @Test
     @DisplayName("오디오 포맷 일치 테스트 성공")
     void formattingTestSuccess() throws UnsupportedAudioFileException, IOException {
-        File file = new File("test.wav");
+        File file = new File("test.wav");//파일 불러오기
         File file2 = new File("formatTest.wav");
 
-        AudioInputStream formatting = Concatenator.formatting(AudioSystem.getAudioInputStream(file));
+        AudioInputStream formatting = Concatenator.formatting(AudioSystem.getAudioInputStream(file));//오디오 포맷 변횐
         AudioInputStream formatting1 = Concatenator.formatting(AudioSystem.getAudioInputStream(file2));
 
         System.out.println("formatting1.getFormat().toString() = " + formatting1.getFormat().toString());
-        assertThat(formatting1.getFormat().toString()).isEqualTo(formatting.getFormat().toString());
+        assertThat(formatting1.getFormat().toString()).isEqualTo(formatting.getFormat().toString());//비교
     }
     @Test
     @DisplayName("오디오 포맷 일치 테스트 실패")
@@ -46,11 +46,11 @@ class ConcatenatorTest {
     @Test
     @DisplayName("오디오 포멧 변경 테스트")
     void formattingTest() throws UnsupportedAudioFileException, IOException {
-        File file = new File("formatTest.wav");
-        AudioInputStream formatting = Concatenator.formatting(AudioSystem.getAudioInputStream(file));
+        File file = new File("formatTest.wav");//오디오 포맷 samplingRate 40000
+        AudioInputStream formatting = Concatenator.formatting(AudioSystem.getAudioInputStream(file));//오디오 포맷 변환
 
         assertThat(formatting.getFormat().toString())
-                .isEqualTo("PCM_SIGNED 44100.0 Hz, 16 bit, stereo, 4 bytes/frame, little-endian");
+                .isEqualTo("PCM_SIGNED 44100.0 Hz, 16 bit, stereo, 4 bytes/frame, little-endian");//변환
 
     }
 }
