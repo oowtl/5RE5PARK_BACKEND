cac79d7c8974818c1c6b811f48d4fe7b372b5582
yunseo <cysdl5604@inu.ac.kr>
2024-11-12 17:24:54 +0900

    [feat] :  TtsProcessHistory 레파지토리 추가

---
+++ src/main/java/com/oreo/finalproject_5re5_be/tts/repository/TtsProcessHistoryRepository.java
@@ -0,0 +1,7 @@
+package com.oreo.finalproject_5re5_be.tts.repository;
+
+import com.oreo.finalproject_5re5_be.tts.entity.TtsProcessHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TtsProcessHistoryRepository extends JpaRepository<TtsProcessHistory, Long> {
+}
