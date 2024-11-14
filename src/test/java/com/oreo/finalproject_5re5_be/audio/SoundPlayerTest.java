8c6071d68cb5c0e5e6dbff1418aa388436938c83
K-KY <kyuyoungk@naver.com>
2024-11-14 14:29:20 +0900

    [Test] : 사운드 플레이어 테스트 수정

--- src/test/java/com/oreo/finalproject_5re5_be/audio/SoundPlayerTest.java
+++ src/test/java/com/oreo/finalproject_5re5_be/audio/SoundPlayerTest.java
@@ -3,18 +3,23 @@ package com.oreo.finalproject_5re5_be.audio;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledOnOs;
 import org.junit.jupiter.api.condition.OS;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 import javax.sound.sampled.*;
-import java.io.ByteArrayInputStream;
 
 import static org.assertj.core.api.Assertions.*;
 
-@SpringBootTest
-@TestPropertySource(locations = "classpath:application-test.properties")
 public class SoundPlayerTest {
     private final SoundPlayer soundPlayer = new SoundPlayer();
+
+    public static final AudioFormat outputFormat = new AudioFormat(
+            AudioFormat.Encoding.PCM_SIGNED,
+            44100, // 44.1kHz로 변환
+            16,    // 16비트
+            2,     // 모노
+            4,     // 2 bytes/frame
+            44100, // frame rate와 샘플링 레이트 일치
+            false  // 리틀 엔디안
+    );
 
     @Test
     @DisabledOnOs({OS.LINUX}) //테스코드가 LINUX 환경에서 돌아가지 않게 해주는 어노테이션
@@ -22,16 +27,9 @@ public class SoundPlayerTest {
 //        File file = new File("");
 //        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
 
-        byte[] audioData = BeepMaker.makeBeep(100, 1.0f);
-        ByteArrayInputStream byteArrayInputStream
-                = new ByteArrayInputStream(BeepMaker.makeBeep(100, 1.0f));
-        AudioFormat format
-                = new AudioFormat(BeepMaker.SAMPLE_RATE, 8, 1, true, false);
-        AudioInputStream audioInputStream =
-                new AudioInputStream(byteArrayInputStream, format, audioData.length / format.getFrameSize());
-
+        AudioInputStream audioData = BeepMaker.makeSound(500, 100, outputFormat);
         //아무 예외도 던지지 않는것을 확인
-        assertThatCode(() -> soundPlayer.play(audioInputStream))
+        assertThatCode(() -> soundPlayer.play(audioData))
                 .doesNotThrowAnyException();
     }
 
