4a6c20bf8cbbd3580a866b62cac98e048ed4bf83
K-KY <kyuyoungk@naver.com>
2024-11-14 15:06:39 +0900

    [Comment] : 주석 추가

--- src/test/java/com/oreo/finalproject_5re5_be/audio/AudioResampleTest.java
+++ src/test/java/com/oreo/finalproject_5re5_be/audio/AudioResampleTest.java
@@ -16,6 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class AudioResampleTest {
 
+    // 모노 오디오 포맷을 60kHz, 16비트로 설정
     private static final AudioFormat monoFormat = new AudioFormat(
             AudioFormat.Encoding.PCM_SIGNED,
             60000, // 60kHz로 변환
@@ -25,6 +26,8 @@ class AudioResampleTest {
             60000, // frame rate와 샘플링 레이트 일치
             false  // 리틀 엔디안
     );
+
+    // 스테레오 오디오 포맷을 60kHz, 16비트로 설정
     private static final AudioFormat stereoFormat = new AudioFormat(
             AudioFormat.Encoding.PCM_SIGNED,
             60000, // 60kHz로 변환
@@ -35,88 +38,121 @@ class AudioResampleTest {
             false  // 리틀 엔디안
     );
 
+    // 모노 리샘플링 설정 객체 생성
     AudioResample audioMonoResample = new AudioResample(monoFormat);
+
+    // 스테레오 리샘플링 설정 객체 생성
     AudioResample audioStereoResample = new AudioResample(stereoFormat);
 
-
-
     @Test
-    @DisplayName("모노 리셈플링 개별 성공 테스트")
+    @DisplayName("모노 리샘플링 개별 성공 테스트")
     void resampleMonoPartSuccess() throws UnsupportedAudioFileException, IOException {
+        // 테스트용 파일 로드
         File file = new File("test.wav");
         AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+
+        // 모노 리샘플링 후 포맷이 기대한 포맷과 일치하는지 확인
         AudioFormat format = audioMonoResample.resample(audioInputStream).getFormat();
         assertThat(format.matches(monoFormat)).isTrue();
     }
+
     @Test
-    @DisplayName("모노 리셈플링 개별 실패 테스트")
+    @DisplayName("모노 리샘플링 개별 실패 테스트")
     void resampleMonoPartFail() throws UnsupportedAudioFileException, IOException {
+        // 테스트용 파일 로드
         File file = new File("test.wav");
         AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+
+        // 모노 리샘플링 후 포맷이 스테레오 포맷과 일치하지 않는지 확인
         AudioFormat format = audioMonoResample.resample(audioInputStream).getFormat();
         assertThat(format.matches(stereoFormat)).isFalse();
     }
 
     @Test
-    @DisplayName("모노 리셈플링 리스트 성공 테스트")
+    @DisplayName("모노 리샘플링 리스트 성공 테스트")
     void resampleMonoSerialSuccess() throws UnsupportedAudioFileException, IOException {
+        // 동일한 테스트 파일 두 개로 리스트 생성
         File file = new File("test.wav");
         File file2 = new File("test.wav");
-        List<AudioInputStream> audioInputStream
-                = List.of(AudioSystem.getAudioInputStream(file), AudioSystem.getAudioInputStream(file2));
+        List<AudioInputStream> audioInputStream = List.of(
+                AudioSystem.getAudioInputStream(file), AudioSystem.getAudioInputStream(file2)
+        );
 
+        // 리스트 내 모든 오디오가 모노 포맷과 일치하는지 확인
         audioMonoResample
                 .resample(audioInputStream)
                 .forEach(ais -> assertThat(ais.getFormat().matches(monoFormat)).isTrue());
     }
+
     @Test
-    @DisplayName("모노 리셈플링 리스트 실패 테스트")
+    @DisplayName("모노 리샘플링 리스트 실패 테스트")
     void resampleMonoSerialFail() throws UnsupportedAudioFileException, IOException {
+        // 동일한 테스트 파일 두 개로 리스트 생성
         File file = new File("test.wav");
         File file2 = new File("test.wav");
-        List<AudioInputStream> audioInputStream
-                = List.of(AudioSystem.getAudioInputStream(file), AudioSystem.getAudioInputStream(file2));
+        List<AudioInputStream> audioInputStream = List.of(
+                AudioSystem.getAudioInputStream(file), AudioSystem.getAudioInputStream(file2)
+        );
 
+        // 리스트 내 모든 오디오가 스테레오 포맷과 일치하지 않는지 확인
         audioMonoResample
                 .resample(audioInputStream)
                 .forEach(ais -> assertThat(ais.getFormat().matches(stereoFormat)).isFalse());
     }
 
     @Test
-    @DisplayName("스테레오 리셈플링 개별 성공 테스트")
+    @DisplayName("스테레오 리샘플링 개별 성공 테스트")
     void resampleStereoPartSuccess() throws UnsupportedAudioFileException, IOException {
+        // 테스트용 파일 로드
         File file = new File("test.wav");
         AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+
+        // 스테레오 리샘플링 후 포맷이 기대한 포맷과 일치하는지 확인
         AudioFormat format = audioStereoResample.resample(audioInputStream).getFormat();
         assertThat(format.matches(stereoFormat)).isTrue();
     }
+
     @Test
-    @DisplayName("스테레오 리셈플링 개별 실패 테스트")
+    @DisplayName("스테레오 리샘플링 개별 실패 테스트")
     void resampleStereoPartFail() throws UnsupportedAudioFileException, IOException {
+        // 테스트용 파일 로드
         File file = new File("test.wav");
         AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+
+        // 스테레오 리샘플링 후 포맷이 모노 포맷과 일치하지 않는지 확인
         AudioFormat format = audioStereoResample.resample(audioInputStream).getFormat();
         assertThat(format.matches(monoFormat)).isFalse();
     }
 
     @Test
-    @DisplayName("스테레오 리셈플링 리스트 성공 테스트")
+    @DisplayName("스테레오 리샘플링 리스트 성공 테스트")
     void resampleStereoSerialSuccess() throws UnsupportedAudioFileException, IOException {
+        // 동일한 테스트 파일 두 개로 리스트 생성
         File file = new File("test.wav");
         File file2 = new File("test.wav");
-        List<AudioInputStream> audioInputStream
-                = List.of(AudioSystem.getAudioInputStream(file), AudioSystem.getAudioInputStream(file2));
+        List<AudioInputStream> audioInputStream = List.of(
+                AudioSystem.getAudioInputStream(file), AudioSystem.getAudioInputStream(file2)
+        );
 
-        audioStereoResample.resample(audioInputStream).forEach(ais -> assertThat(ais.getFormat().matches(stereoFormat)).isTrue());
+        // 리스트 내 모든 오디오가 스테레오 포맷과 일치하는지 확인
+        audioStereoResample
+                .resample(audioInputStream)
+                .forEach(ais -> assertThat(ais.getFormat().matches(stereoFormat)).isTrue());
     }
+
     @Test
-    @DisplayName("스테레오 리셈플링 리스트 실패 테스트")
+    @DisplayName("스테레오 리샘플링 리스트 실패 테스트")
     void resampleStereoSerialFail() throws UnsupportedAudioFileException, IOException {
+        // 동일한 테스트 파일 두 개로 리스트 생성
         File file = new File("test.wav");
         File file2 = new File("test.wav");
-        List<AudioInputStream> audioInputStream
-                = List.of(AudioSystem.getAudioInputStream(file), AudioSystem.getAudioInputStream(file2));
+        List<AudioInputStream> audioInputStream = List.of(
+                AudioSystem.getAudioInputStream(file), AudioSystem.getAudioInputStream(file2)
+        );
 
-        audioStereoResample.resample(audioInputStream).forEach(ais -> assertThat(ais.getFormat().matches(monoFormat)).isFalse());
+        // 리스트 내 모든 오디오가 모노 포맷과 일치하지 않는지 확인
+        audioStereoResample
+                .resample(audioInputStream)
+                .forEach(ais -> assertThat(ais.getFormat().matches(monoFormat)).isFalse());
     }
 }
