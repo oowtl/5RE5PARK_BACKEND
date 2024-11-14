e158fc77001786848d6008ede0ad822f878bf51e
yunseo <cysdl5604@inu.ac.kr>
2024-11-14 16:46:04 +0900

    [fix] : 리팩토링에 따른 테스트 코드 수정

--- src/test/java/com/oreo/finalproject_5re5_be/tts/client/GoogleTTSServiceTest.java
+++ src/test/java/com/oreo/finalproject_5re5_be/tts/client/GoogleTTSServiceTest.java
@@ -1,24 +1,32 @@
 package com.oreo.finalproject_5re5_be.tts.client;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class GoogleTTSClientTest {
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-ndb-test.properties")
+class GoogleTTSServiceTest {
+    @Autowired
+    GoogleTTSService googleTTSService;
+
     @Test
     public void makeTest() throws Exception {
         // 입력 파라미터 세팅
         String text = "Hello, world";
         String languageCode = "nl-NL";
         String voiceName = "nl-NL-Standard-D";
-
-        GoogleTTSClient ttsClient = new GoogleTTSClient(text, languageCode, voiceName);
-        ttsClient.setPitch(2.0);
-        ttsClient.setSpeed(2.0);
-        ttsClient.setVoiceGender("male");
+        String gender = "female";
 
         // tts 생성 요청
-        byte[] audioByteArray = ttsClient.make();
+        byte[] audioByteArray = googleTTSService.make(
+                SynthesisInputGenerator.generate(text),
+                VoiceParamsGenerator.generate(languageCode, voiceName, gender),
+                AudioConfigGenerator.generate(1.0, 0.0, 0.0)
+        );
 
         // tts 생성 결과가 반환되어야 함
         assertTrue(audioByteArray != null);
