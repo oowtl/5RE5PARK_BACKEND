f7738b3834b51343d3afa0a785397fb26e1bc215
K-KY <kyuyoungk@naver.com>
2024-11-15 14:21:53 +0900

    [chore] : 오디오 채널 상수 클래스 추가

---
+++ src/main/java/com/oreo/finalproject_5re5_be/audio/AudioChannels.java
@@ -0,0 +1,8 @@
+package com.oreo.finalproject_5re5_be.audio;
+
+public class AudioChannels {
+    private AudioChannels() {}
+
+    public static final int MONO = 1;
+    public static final int STEREO = 2;
+}
