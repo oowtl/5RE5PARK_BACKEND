0c1cf63fc3947a923e6c30769bde158f9138f596
alswooy <108874185+alswooy@users.noreply.github.com>
2024-11-15 16:15:43 +0900

    [fix] project service 변경
    - 서비스 리턴값타입, 매개변수 타입 변경
    - delete 리스트로 변경

--- src/main/java/com/oreo/finalproject_5re5_be/project/service/ProjectServiceImpl.java
+++ src/main/java/com/oreo/finalproject_5re5_be/project/service/ProjectServiceImpl.java
@@ -6,11 +6,10 @@ import com.oreo.finalproject_5re5_be.project.dto.response.ProjectResponse;
 import com.oreo.finalproject_5re5_be.project.entity.Project;
 import com.oreo.finalproject_5re5_be.project.exception.InvalidProjectNameException;
 import com.oreo.finalproject_5re5_be.project.repository.ProjectRepository;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
@@ -51,23 +50,18 @@ public class ProjectServiceImpl implements ProjectService {
     }
 
     @Override
-    public void projectSave(String projectName) {
-        validateProjectName(projectName);
+    public Long projectSave() {
         Member member = getCurrentUser();
         Project project = Project.builder()
                 .member(member)
                 .build();
-        if(projectName.length()>0){
-            project = Project.builder()
-                    .member(member)
-                    .proName(projectName)
-                    .build();
-        }
-        projectRepository.save(project);
+        Project save = projectRepository.save(project);
+        log.info("Save project : {}", save);
+        return save.getProSeq();
     }
 
     @Override
-    public void projectUpdate(Long projectSeq, String projectName) {
+    public void projectUpdate(@NotNull Long projectSeq,@NotNull String projectName) {
         validateProjectName(projectName);
         Project projectFind = projectRepository.findById(projectSeq)
                 .orElseThrow(() -> new IllegalArgumentException("src file not found"));
@@ -80,28 +74,31 @@ public class ProjectServiceImpl implements ProjectService {
 
 
     @Override
-    public void projectDelete(Long projectSeq) {
-        Project projectFind = projectRepository.findById(projectSeq)
-                .orElseThrow(() -> new IllegalArgumentException("src file not found"));
-        Project project = projectFind.toBuilder()
-                .proSeq(projectSeq)
-                .proActivate('N')
-                .build();
-        projectRepository.save(project);
+    public void projectDelete(List<Long> projectSeq) {
+        for (int i = 0; i < projectSeq.size(); i++) {
+            Project projectFind = projectRepository.findById(projectSeq.get(i))
+                    .orElseThrow(() -> new IllegalArgumentException("src file not found"));
+            Project project = projectFind.toBuilder()
+                    .proSeq(projectSeq.get(i))
+                    .proActivate('N')
+                    .build();
+            projectRepository.save(project);
+        }
     }
 
-    private void validateProjectName(String projectName) {
+    private void validateProjectName(@NotNull String projectName) {
         if (projectName == null || projectName.length() < 3 || projectName.length() > 50) {
             throw new InvalidProjectNameException("프로젝트 이름은 3자 이상, 50자 이하여야 합니다.");
         }
     }
     private Member getCurrentUser() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String userId = authentication.getName();
-        Member member = memberRepository.findById(userId);
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        String userId = authentication.getName();
+        Member member = memberRepository.findById("akbvFQMtnk2WFjgngJH");
         if(member==null){
             throw new UsernameNotFoundException("유저 정보가 없습니다.");
         }
+
         return member;
     }
 }
