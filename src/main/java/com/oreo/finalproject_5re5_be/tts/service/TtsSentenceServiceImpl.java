9a6da4344a412c9b9c7ae3f64dd2a426bc14227b
oowtl <oowtl7425@gmail.com>
2024-11-16 13:45:02 +0900

    [feat] TtsSentence 수정 기능
    1. TtsSentenceUpdateRequest (수정용 DTO)
    2가지의 요청에 대한 DTO 가 겹칩니다. 이후에 리팩토링하는 것으로 결정하고 작성하도록 하겠습니다.

--- src/main/java/com/oreo/finalproject_5re5_be/tts/service/TtsSentenceServiceImpl.java
+++ src/main/java/com/oreo/finalproject_5re5_be/tts/service/TtsSentenceServiceImpl.java
@@ -1,9 +1,11 @@
 package com.oreo.finalproject_5re5_be.tts.service;
 
+import com.oreo.finalproject_5re5_be.global.exception.EntityNotFoundException;
 import com.oreo.finalproject_5re5_be.project.entity.Project;
 import com.oreo.finalproject_5re5_be.project.repository.ProjectRepository;
 import com.oreo.finalproject_5re5_be.tts.dto.request.TtsAttributeInfo;
 import com.oreo.finalproject_5re5_be.tts.dto.request.TtsSentenceCreateRequest;
+import com.oreo.finalproject_5re5_be.tts.dto.request.TtsSentenceUpdateRequest;
 import com.oreo.finalproject_5re5_be.tts.dto.response.TtsSentenceDto;
 import com.oreo.finalproject_5re5_be.tts.entity.*;
 import com.oreo.finalproject_5re5_be.tts.repository.*;
@@ -90,5 +92,64 @@ public class TtsSentenceServiceImpl implements TtsSentenceService {
 
         // 6. TtsSentenceResponse 변환
         return TtsSentenceDto.of(createdTtsSentence);
+    }
+
+    /**
+     * @param projectSeq
+     * @param updateRequest
+     * @return
+     * @apiNote TtsSentence 엔티티 수정
+     */
+    @Override
+    public TtsSentenceDto updateSentence(@Valid @NotNull Long projectSeq, @Valid TtsSentenceUpdateRequest updateRequest) {
+        // 1. TtsSentenceUpdateRequest 유효성 검증
+        if (updateRequest == null) {
+            throw new IllegalArgumentException("Update request cannot be null");
+        }
+
+        // 2. DB 유효성 검증
+        // 2.1 projectSeq 조회 가능한 projectSeq (존재 여부) 검증 및 할당
+        Project project = projectRepository.findById(projectSeq)
+                .orElseThrow(() -> new EntityNotFoundException("Project not found with id: " + projectSeq));
+
+        // 2.2 voiceSeq 조회 가능한 voiceSeq (존재 여부) 검증 및 할당
+        Voice voice = voiceRepository.findById(updateRequest.getVoiceSeq())
+                .orElseThrow(() -> new EntityNotFoundException("Voice not found with id: " + updateRequest.getVoiceSeq()));
+
+        // 2.3 styleSeq 조회 가능한 styleSeq (존재 여부) 검증 및 할당
+        Style style = styleRepository.findById(updateRequest.getStyleSeq())
+                .orElseThrow(() -> new EntityNotFoundException("Style not found with id: " + updateRequest.getStyleSeq()));
+
+        // 3. TtsSentenceUpdateRequest -> TtsSentence 변환
+        // 3.1 TtsSentence 엔티티 조회
+        TtsSentence sentence = ttsSentenceRepository.findById(updateRequest.getTsSeq())
+                .orElseThrow(() -> new EntityNotFoundException("TtsSentence not found with id: " + updateRequest.getTsSeq()));
+
+        // 3.2 TtsSentence 엔티티 수정
+        TtsSentence updateSentence = sentence.toBuilder()
+                .text(updateRequest.getText())
+                .voice(voice)
+                .style(style)
+                .sortOrder(updateRequest.getOrder())
+                .volume(updateRequest.getAttribute().getVolume())
+                .speed(updateRequest.getAttribute().getSpeed())
+                .startPitch(updateRequest.getAttribute().getStPitch())
+                .emotion(updateRequest.getAttribute().getEmotion())
+                .emotionStrength(updateRequest.getAttribute().getEmotionStrength())
+                .sampleRate(updateRequest.getAttribute().getSampleRate())
+                .alpha(updateRequest.getAttribute().getAlpha())
+                .endPitch(updateRequest.getAttribute().getEndPitch())
+                .audioFormat(updateRequest.getAttribute().getAudioFormat())
+                .ttsAudiofile(null) // 3.3 TtsSentence 에 연관된 ttsAudioFile 연결 끊기
+                .build();
+
+
+        // 4. TtsSentence 저장
+        TtsSentence updatedSentence = ttsSentenceRepository.save(updateSentence);
+
+        System.out.println("updatedSentence = " + updatedSentence);
+
+        // 5. TtsSentenceResponse 변환
+        return TtsSentenceDto.of(updatedSentence);
     }
 }
