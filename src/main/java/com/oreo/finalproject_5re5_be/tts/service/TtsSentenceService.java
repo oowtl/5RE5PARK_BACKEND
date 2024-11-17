4972e86c369f99b2fe21850f1bc6b853ee0704f4
oowtl <oowtl7425@gmail.com>
2024-11-17 20:21:12 +0900

    [feat] TtsSentenceService.batchSaveSentence 구현

--- src/main/java/com/oreo/finalproject_5re5_be/tts/service/TtsSentenceService.java
+++ src/main/java/com/oreo/finalproject_5re5_be/tts/service/TtsSentenceService.java
@@ -1,11 +1,14 @@
 package com.oreo.finalproject_5re5_be.tts.service;
 
 import com.oreo.finalproject_5re5_be.tts.dto.request.TtsSentenceRequest;
+import com.oreo.finalproject_5re5_be.tts.dto.request.TtsSentenceBatchRequest;
 import com.oreo.finalproject_5re5_be.tts.dto.response.TtsSentenceDto;
+import com.oreo.finalproject_5re5_be.tts.dto.response.TtsSentenceListDto;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
 public interface TtsSentenceService {
     TtsSentenceDto addSentence(@Valid @NotNull Long projectSeq, @Valid TtsSentenceRequest createRequest);
     TtsSentenceDto updateSentence(@Valid @NotNull Long projectSeq, @Valid @NotNull Long tsSeq, @Valid TtsSentenceRequest updateRequest);
+    TtsSentenceListDto batchSaveSentence(@Valid @NotNull Long projectSeq, @Valid TtsSentenceBatchRequest batchRequest);
 }
