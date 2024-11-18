1bd73a895f5e2302d996fff63cd0b8886a9775b4
jongheonleee <qwefghnm1212@gmail.com>
2024-11-18 11:01:32 +0900

    [feat] 회원 수정에 필요한 Repository 작업 구현

--- src/main/java/com/oreo/finalproject_5re5_be/member/repository/MemberRepository.java
+++ src/main/java/com/oreo/finalproject_5re5_be/member/repository/MemberRepository.java
@@ -2,10 +2,26 @@ package com.oreo.finalproject_5re5_be.member.repository;
 
 import com.oreo.finalproject_5re5_be.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     public Member findByEmail(String email);
     public Member findById(String id);
     public boolean existsByEmail(String email);
+    public boolean existsById(String id);
+    @Query( "SELECT CASE WHEN COUNT(m) > 0 THEN true ELSE false END " +
+            "FROM Member m " +
+            "WHERE m.seq <> :memberSeq " +
+            "AND m.id = :id" )
+    public boolean existsByIdNotContainingMemberSeq(@Param("memberSeq") Long memberSeq, @Param("id") String id);
+
+    @Query( "SELECT CASE WHEN COUNT(m) > 0 THEN true ELSE false END " +
+            "FROM Member m " +
+            "WHERE m.seq <> :memberSeq " +
+            "AND m.email = :email" )
+    public boolean existsByEmailNotContainingMemberSeq(@Param("memberSeq")Long memberSeq, @Param("email") String email);
+
+
 }
