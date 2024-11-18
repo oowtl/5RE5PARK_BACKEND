2b8be66c1642b1bd41808ed9f0cc5972ede2fea5
alswooy <108874185+alswooy@users.noreply.github.com>
2024-11-18 21:07:18 +0900

    [fix] VcApiServiceImplTest CI 환경 테스트 안되게 변경

--- src/test/java/com/oreo/finalproject_5re5_be/vc/service/VcApiServiceImplTest.java
+++ src/test/java/com/oreo/finalproject_5re5_be/vc/service/VcApiServiceImplTest.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
@@ -22,6 +24,7 @@ class VcApiServiceImplTest {
     private VcApiService vcApiService;
 
     @Test
+    @DisabledOnOs({OS.LINUX})
     @DisplayName("타겟파일을 ID로 변환 시키는 테스트")
     @Disabled//빌드 테스트 동작되지 않게 설정 API 요청되지 않게
     void trgIdCreate() throws IOException {
@@ -40,11 +43,12 @@ class VcApiServiceImplTest {
 
     @Test
     @Disabled
+    @DisabledOnOs({OS.LINUX})
     @DisplayName("타겟 ID와 소스파일로 결과 파일을 만드는 테스트 코드")
     void resultCreate() throws IOException {
-        FileInputStream inputStream = new FileInputStream("ttsoutput.mp3");
+        FileInputStream inputStream = new FileInputStream("");// TRG가 되는 파일
 
-        // MockMultipartFile 생성
+        // MockMultipartFile 으로 변환
         MultipartFile multipartFile = new MockMultipartFile(
                 "test",                       // 필드 이름
                 "test.wav",                   // 파일 이름
@@ -52,7 +56,7 @@ class VcApiServiceImplTest {
                 inputStream                   // 파일 데이터
         );
         MultipartFile file = vcApiService.resultFileCreate(multipartFile, "Yr8eh9CaB3tXeZD1ogAu");
-        saveMultipartFileToProjectRoot(file, "result.mp3");
+        saveMultipartFileToProjectRoot(file, "");
     }
     //파일 저장 테스트 확인용
     public String saveMultipartFileToProjectRoot(MultipartFile file, String fileName) throws IOException {
@@ -72,9 +76,10 @@ class VcApiServiceImplTest {
     }
     @Test
     @Disabled
+    @DisabledOnOs({OS.LINUX})
     @DisplayName("타겟파일을 ID로 만들고 ID와 소스파일로 결과 파일을 만드는 테스트 코드")
     public void resultTest() throws IOException {
-        FileInputStream inputStream = new FileInputStream("mario.mp3");//Trg 파일 입력
+        FileInputStream inputStream = new FileInputStream("");//Trg 파일 입력
 
         // MockMultipartFile 생성
         MultipartFile multipartFile = new MockMultipartFile(
@@ -85,7 +90,7 @@ class VcApiServiceImplTest {
         );
         String s = vcApiService.trgIdCreate(multipartFile);//trg ID 생성
 
-        FileInputStream inputStream2 = new FileInputStream("ttsoutput.mp3");//소스파일 입력
+        FileInputStream inputStream2 = new FileInputStream("");//소스파일 입력
 
         // MockMultipartFile 생성
         MultipartFile multipartFile2 = new MockMultipartFile(
@@ -95,6 +100,6 @@ class VcApiServiceImplTest {
                 inputStream2                  // 파일 데이터
         );
         MultipartFile file = vcApiService.resultFileCreate(multipartFile2, s);//Trg Id 소스파일로 결과 파일 생성
-        saveMultipartFileToProjectRoot(file, "result.mp3");
+        saveMultipartFileToProjectRoot(file, "result.mp3");//파일 저장
     }
 }
