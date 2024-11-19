c40b9de96bc1e683074bb4e0974ce790d5428334
yschoi123 <cysdl5604@inu.ac.kr>
2024-11-19 18:37:13 +0900

    [feat] 언어 조회 응답 DTO 추가

---
+++ src/main/java/com/oreo/finalproject_5re5_be/tts/dto/response/LanguageDto.java
@@ -0,0 +1,28 @@
+package com.oreo.finalproject_5re5_be.tts.dto.response;
+
+import com.oreo.finalproject_5re5_be.tts.entity.Language;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LanguageDto {
+    private String languageCode;
+    private String languageName;
+    private String regionCode;
+    private String regionName;
+
+    public static LanguageDto of(Language language) {
+        return LanguageDto.builder()
+                .languageCode(language.getLangCode())
+                .languageName(language.getLangName())
+                .regionCode(language.getRegionCode())
+                .regionName(language.getRegionName())
+                .build();
+    }
+
+}
