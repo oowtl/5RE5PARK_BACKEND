f54f248e9f784f7c8c2c27e0dcdbec96373883e5
yschoi123 <cysdl5604@inu.ac.kr>
2024-11-19 18:38:25 +0900

    [feat] 언어 전체 조회 서비스 추가

---
+++ src/main/java/com/oreo/finalproject_5re5_be/tts/service/LanguageService.java
@@ -0,0 +1,25 @@
+package com.oreo.finalproject_5re5_be.tts.service;
+
+import com.oreo.finalproject_5re5_be.tts.dto.response.LanguageListDto;
+import com.oreo.finalproject_5re5_be.tts.entity.Language;
+import com.oreo.finalproject_5re5_be.tts.repository.LanguageRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LanguageService {
+    private final LanguageRepository languageRepository;
+
+    public LanguageService(LanguageRepository languageRepository) {
+        this.languageRepository = languageRepository;
+    }
+
+    public LanguageListDto getLanguageList() {
+        // 전체 언어 목록 조회
+        List<Language> languageList = languageRepository.findAll();
+
+        // 언어 목록 응답 형태로 변환하여 반환
+        return LanguageListDto.of(languageList);
+    }
+}
