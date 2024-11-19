967c23df9a10d98c53dbca5dfc19318c3c68db88
yschoi123 <cysdl5604@inu.ac.kr>
2024-11-19 18:37:26 +0900

    [feat] 언어 목록 조회 응답 DTO 추가

---
+++ src/main/java/com/oreo/finalproject_5re5_be/tts/dto/response/LanguageListDto.java
@@ -0,0 +1,28 @@
+package com.oreo.finalproject_5re5_be.tts.dto.response;
+
+import com.oreo.finalproject_5re5_be.tts.entity.Language;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LanguageListDto {
+    private List<LanguageDto> languageList;
+
+
+    public static LanguageListDto of(List<Language> languageList) {
+        List<LanguageDto> list = languageList.stream()
+                .map(LanguageDto::of)
+                .toList();
+
+        return LanguageListDto.builder()
+                .languageList(list)
+                .build();
+    }
+}
