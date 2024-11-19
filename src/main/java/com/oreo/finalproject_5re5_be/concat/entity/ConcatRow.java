30e9ba6c54748e79741a33fd6cb892fb55bc40f8
K-KY <kyuyoungk@naver.com>
2024-11-20 03:40:43 +0900

    [Feat] : 배치 처리를 위한 ConcatRow 테이블 수정
    ### 이외 변경사항
    - "concet"_row_seq 오타 수정
    - 칼럼 이름 명시
    - 아이디 생성 전략 변경

--- src/main/java/com/oreo/finalproject_5re5_be/concat/entity/ConcatRow.java
+++ src/main/java/com/oreo/finalproject_5re5_be/concat/entity/ConcatRow.java
@@ -1,35 +1,41 @@
 package com.oreo.finalproject_5re5_be.concat.entity;
 
 import com.oreo.finalproject_5re5_be.global.entity.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.*;
 
 @ToString
 @Getter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity(name = "concat_row")
 public class ConcatRow extends BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "concet_row_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "concat_row_seq_generator")
+    @SequenceGenerator(
+            name = "concat_row_seq_generator",
+            sequenceName = "concat_row_seq", // 실제 시퀀스 이름
+            allocationSize = 1              // ID를 하나씩 할당
+    )
+    @Column(name = "concat_row_seq")
     private Long concatRowSeq;
+
 
     @ManyToOne
     @JoinColumn(name = "pro_seq")
     private ConcatTab concatTab;
 
+    @Column(name = "row_text")
     private String rowText;
+    @Column(name = "selected")
     private Character selected;
+    @Column(name = "silence")
     private Float silence;
+    @Column(name = "row_index")
     private Integer rowIndex;
+    @Column(name = "status")
     private Character status;
 
 
