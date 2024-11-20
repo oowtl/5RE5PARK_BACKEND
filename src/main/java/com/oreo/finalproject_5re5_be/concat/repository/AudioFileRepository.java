28256d2ad35d020dca9ec104147cd60e1854168f
Ryu-Eun <ektl4m@gmail.com>
2024-11-20 15:10:10 +0900

    [feat] Repository 메서드 추가

--- src/main/java/com/oreo/finalproject_5re5_be/concat/repository/AudioFileRepository.java
+++ src/main/java/com/oreo/finalproject_5re5_be/concat/repository/AudioFileRepository.java
@@ -1,9 +1,36 @@
 package com.oreo.finalproject_5re5_be.concat.repository;
 
 import com.oreo.finalproject_5re5_be.concat.entity.AudioFile;
+import com.oreo.finalproject_5re5_be.concat.entity.ConcatRow;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AudioFileRepository extends JpaRepository<AudioFile, Long> {
+    @Query("SELECT cr FROM concat_row cr WHERE cr.audioFile.audioFileSeq = :audioFileSeq")
+    ConcatRow findConcatRowByAudioFile(Long audioFileSeq);
+
+    Optional<AudioFile> findByAudioUrl(String audioUrl);
+
+    Optional<AudioFile> findByFileName(String filename);
+
+    @Query("SELECT af FROM audio_file af WHERE af.concatRow.concatRowSeq = :concatRowSeq")
+    Optional<AudioFile> findByRowSeq(Long concatRowSeq);
+
+    @Query("SELECT af FROM audio_file af WHERE FUNCTION('DATE', af.createdDate) = :date")
+    List<AudioFile> findByCreatedDateOnly(LocalDate date);
+
+    //페이징 처리해서 데이터를 한번에 모두 가져오지 않고 필요한 만큼만 나눠서 처리
+    Page<AudioFile> findByExtension(String extension, Pageable pageable);
+
+
+    void deleteById(Long seq);
+
 }
