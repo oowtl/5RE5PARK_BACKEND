5c161f77b4c772834785b1244a9aa8075984ab1a
Ryu-Eun <ektl4m@gmail.com>
2024-11-20 21:17:55 +0900

    [feat] dto 추가

---
+++ src/main/java/com/oreo/finalproject_5re5_be/concat/dto/request/AudioFormatRequest.java
@@ -0,0 +1,22 @@
+package com.oreo.finalproject_5re5_be.concat.dto.request;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class AudioFormatRequest {
+
+    private String encoding;
+    private Integer sampleRate;
+    private Short sampleSizeBit;
+    private Short channel;
+    private Integer frameSize;
+    private Short frameRate;
+    private Character isBigEndian;
+
+}
