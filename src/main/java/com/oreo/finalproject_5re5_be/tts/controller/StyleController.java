ab2cc2ba516adf4e125fdb2d48ae6b17d0ae35fe
yunseo <cysdl5604@inu.ac.kr>
2024-11-21 16:49:35 +0900

    [chore] : Swagger 어노테이션 추가

--- src/main/java/com/oreo/finalproject_5re5_be/tts/controller/StyleController.java
+++ src/main/java/com/oreo/finalproject_5re5_be/tts/controller/StyleController.java
@@ -3,12 +3,15 @@ package com.oreo.finalproject_5re5_be.tts.controller;
 import com.oreo.finalproject_5re5_be.global.dto.response.ResponseDto;
 import com.oreo.finalproject_5re5_be.tts.dto.response.StyleListDto;
 import com.oreo.finalproject_5re5_be.tts.service.StyleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "TTS-Voice", description = "Voice의 Style 관련 API")
 @RestController
 @RequestMapping("/api/style")
 public class StyleController {
@@ -19,6 +22,7 @@ public class StyleController {
         this.styleService = styleService;
     }
 
+    @Operation(summary = "voice 스타일 전체 조회")
     @GetMapping("")
     public ResponseEntity<ResponseDto<StyleListDto>> getStyleList() {
 
