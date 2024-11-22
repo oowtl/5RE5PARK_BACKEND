a5681754576ba36026543483916a2c394aa24497
K-KY <kyuyoungk@naver.com>
2024-11-23 08:36:12 +0900

    [Feat] : AudioFile insert 배치 추가

---
+++ src/main/java/com/oreo/finalproject_5re5_be/concat/service/helper/AudioFileHelper.java
@@ -0,0 +1,29 @@
+package com.oreo.finalproject_5re5_be.concat.service.helper;
+
+import com.oreo.finalproject_5re5_be.concat.entity.AudioFile;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class AudioFileHelper {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    public void batchInsert(List<AudioFile> rows) {
+        for (int i = 0; i < rows.size(); i++) {
+            entityManager.persist(rows.get(i));
+            if (i > 0 && i % 20 == 0) { // 20개마다 flush & clear
+                entityManager.flush();
+                entityManager.clear();
+            }
+        }
+        entityManager.flush();
+        entityManager.clear();
+    }
+
+}
