567e89ee9a9ed7fa68cc5bb8b71e82a95f9e3ac5
K-KY <kyuyoungk@naver.com>
2024-11-23 08:30:53 +0900

    [Feat] : 멀티파트 파일을 dto로 변환하기 위한 설정 파일

---
+++ src/main/java/com/oreo/finalproject_5re5_be/global/config/MultipartFileAccepter.java
@@ -0,0 +1,18 @@
+package com.oreo.finalproject_5re5_be.global.config;
+
+import com.oreo.finalproject_5re5_be.concat.dto.request.AudioFileRequestDto;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class MultipartFileAccepter implements Converter<MultipartFile, AudioFileRequestDto> {
+
+    @SuppressWarnings("NullableProblems")
+    @Override
+    public AudioFileRequestDto convert(MultipartFile source) {
+        return AudioFileRequestDto.builder()
+                .audioFile(source)
+                .build();
+    }
+}
