1743f187b40618b64447b3f4babc4c2578bf35d9
yunseo <cysdl5604@inu.ac.kr>
2024-11-23 00:05:02 +0900

    [feat] : concat row 리스트 조회 응답 DTO 추가

---
+++ src/main/java/com/oreo/finalproject_5re5_be/concat/dto/ConcatRowListDto.java
@@ -0,0 +1,27 @@
+package com.oreo.finalproject_5re5_be.concat.dto;
+
+import com.oreo.finalproject_5re5_be.concat.entity.ConcatRow;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Builder
+@ToString
+public class ConcatRowListDto {
+    private List<ConcatRowDto> rowList;
+
+    // concatRow 엔티티 리스트 정보로 ConcatRowDto 객체 생성하는 메서드
+    public static ConcatRowListDto of(List<ConcatRow> concatRowEntityList) {
+        // concatRow 엔티티 리스트를 ConcatRowDto 리스트로 변환
+        List<ConcatRowDto> concatRowDtoLList =
+                concatRowEntityList.stream().map(ConcatRowDto::of).toList();
+
+        // ConcatRowDto 리스트를 넣으며 ConcatRowListDto 생성
+        return ConcatRowListDto.builder()
+                .rowList(concatRowDtoLList)
+                .build();
+    }
+}
