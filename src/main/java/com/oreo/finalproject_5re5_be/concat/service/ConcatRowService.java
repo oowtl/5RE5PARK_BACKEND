0f251f3d0250466eff0d718f73a8ca57257dc508
K-KY <kyuyoungk@naver.com>
2024-11-23 08:55:03 +0900

    [Feat] : ConcatRow 저장 로직 추가

--- src/main/java/com/oreo/finalproject_5re5_be/concat/service/ConcatRowService.java
+++ src/main/java/com/oreo/finalproject_5re5_be/concat/service/ConcatRowService.java
@@ -1,7 +1,10 @@
 package com.oreo.finalproject_5re5_be.concat.service;
 
 import com.oreo.finalproject_5re5_be.concat.dto.ConcatRowDto;
+import com.oreo.finalproject_5re5_be.concat.dto.request.ConcatRowRequest;
 import com.oreo.finalproject_5re5_be.concat.dto.request.ConcatRowSaveRequestDto;
+import com.oreo.finalproject_5re5_be.concat.dto.request.OriginAudioRequest;
+import com.oreo.finalproject_5re5_be.concat.entity.AudioFile;
 import com.oreo.finalproject_5re5_be.concat.entity.ConcatRow;
 import com.oreo.finalproject_5re5_be.concat.entity.ConcatTab;
 import com.oreo.finalproject_5re5_be.concat.repository.ConcatRowRepository;
@@ -11,41 +14,17 @@ import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 @AllArgsConstructor
-
 public class ConcatRowService {
     private ConcatRowRepository concatRowRepository;
     private ConcatTabRepository concatTabRepository;
     private final ConcatRowHelper concatRowHelper;
+    private final AudioFileService audioFileService;
 
     //현재 Hibernate에서 데이터 무결성 확인을 위해 자동으로 project, concat_tab, concat_option을 확인 하는 쿼리를 생성함
-    //
-    @Transactional
-    public boolean saveConcatRows(ConcatRowSaveRequestDto requestDto) {
-        Optional<ConcatTab> concatTabOpt = concatTabRepository.findById(requestDto.getConcatTabId());
-
-        if (concatTabOpt.isPresent()) {
-            ConcatTab concatTab = concatTabOpt.get();
-            List<ConcatRow> concatRowStream = requestDto.getConcatRowRequests()
-                    .stream().map(crr -> ConcatRow.builder()
-                            .concatTab(concatTab)
-                            .rowText(crr.getRowText())
-                            .selected(crr.getSelected())
-                            .status(crr.getStatus())
-                            .silence(crr.getSilence())
-                            .rowIndex(crr.getRowIndex())
-                            .build()).toList();
-            concatRowHelper.batchInsert(concatRowStream);
-            return true;
-        }
-        return false;
-    }
-
     public List<ConcatRowDto> readRecentConcatRows(long projectSequence) {
         List<ConcatRow> concatRows = concatRowRepository
                 .findByStatusAndConcatTab_Project_ProSeq('Y', projectSequence);
@@ -54,9 +33,11 @@ public class ConcatRowService {
         }
         return concatRows.stream().map(cr -> ConcatRowDto.builder()
                 .concatRowSequence(cr.getConcatRowSeq())
+                .projectSequence(projectSequence)
                 .rowText(cr.getRowText())
                 .rowIndex(cr.getRowIndex())
                 .selected(cr.getSelected())
+                .status(cr.getStatus())
                 .silence(cr.getSilence())
                 .build()
         ).toList();
@@ -65,9 +46,6 @@ public class ConcatRowService {
     public List<ConcatRowDto> readConcatRows(long concatRowSequence) {
         List<ConcatRow> concatRows = concatRowRepository
                 .findByConcatRowSeq(concatRowSequence);
-        if (concatRows.isEmpty()) {
-            return new ArrayList<>();
-        }
         return concatRows.stream().map(cr -> ConcatRowDto.builder()
                 .concatRowSequence(cr.getConcatRowSeq())
                 .rowText(cr.getRowText())
@@ -79,17 +57,10 @@ public class ConcatRowService {
     }
 
     @Transactional
-    public boolean disableConcatRows(ConcatRowSaveRequestDto requestDto) {
-        Optional<ConcatTab> concatTabOpt = concatTabRepository.findById(requestDto.getConcatTabId());
+    public boolean disableConcatRows(List<Long> rowSeq) {
 
-        if (concatTabOpt.isPresent()) {
-
-            List<Long> concatRowSeq = requestDto.getConcatRowRequests()
-                    .stream().map(ConcatRowDto::getConcatRowSequence).toList();
-            concatRowRepository.updateStatusByConcatRowSeq(concatRowSeq, 'N');//행 비활성 처리
-            return true;
-        }
-        return false;
+        concatRowRepository.updateStatusByConcatRowSeq(rowSeq, 'N');//행 비활성 처리
+        return true;
     }
 
 
@@ -98,33 +69,69 @@ public class ConcatRowService {
         Optional<ConcatTab> concatTabOpt = concatTabRepository.findById(requestDto.getConcatTabId());
 
         if (concatTabOpt.isPresent()) {
-            ConcatTab concatTab = concatTabOpt.get();
             return disableConcatRowsForUpdate(requestDto.getConcatRowRequests()) &&
-                    createConcatRowsForUpdate(requestDto.getConcatRowRequests(), concatTab);
+                    saveConcatRows(requestDto);
         }
         return false;
     }
 
-    private boolean createConcatRowsForUpdate(List<ConcatRowDto> requestDto, ConcatTab concatTab) {
-        List<ConcatRow> concatRowStream = requestDto
-                .stream().filter(cr -> cr.getStatus() != 'N')
-                .map(crr -> ConcatRow.builder()
-                        .concatTab(concatTab)
-                        .rowText(crr.getRowText())
-                        .selected(crr.getSelected())
-                        .status(crr.getStatus())
-                        .silence(crr.getSilence())
-                        .rowIndex(crr.getRowIndex())
-                        .build()).toList();
-        concatRowHelper.batchInsert(concatRowStream);
-        return true;
-    }
-
-    private boolean disableConcatRowsForUpdate(List<ConcatRowDto> concatRows) {
+    private boolean disableConcatRowsForUpdate(List<ConcatRowRequest> concatRows) {
 
         List<Long> concatRowSeq = concatRows
-                .stream().map(ConcatRowDto::getConcatRowSequence).toList();
+                .stream().map(ConcatRowRequest::getSeq).toList();
         //행 비활성 처리
         return concatRowRepository.updateStatusByConcatRowSeq(concatRowSeq, 'N') != 0;
     }
+
+    public ConcatRow readConcatRow(Long concatRowSeq) {
+        return concatRowRepository.findById(concatRowSeq).orElse(null);
+    }
+
+
+    @Transactional
+    public boolean saveConcatRows(ConcatRowSaveRequestDto requestDto) {
+        // 1. ConcatTab 확인 및 조회
+        ConcatTab concatTab = concatTabRepository.findById(requestDto.getConcatTabId())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid ConcatTab ID: " + requestDto.getConcatTabId()));
+
+        List<AudioFile> audioFiles = new ArrayList<>();
+        // 2. ConcatRowRequest 처리
+        List<ConcatRow> concatRows = requestDto.getConcatRowRequests().stream()
+                .filter(rowRequest -> rowRequest.getStatus() != 'N').map(rowRequest -> {
+
+                    // 2.2 ConcatRow 생성
+                    ConcatRow concatRow = ConcatRow.builder()
+                            .concatTab(concatTab)
+                            .rowText(rowRequest.getRowText())
+                            .selected(rowRequest.getSelected())
+                            .silence(rowRequest.getRowSilence())
+                            .rowIndex(rowRequest.getRowIndex())
+                            .status('Y') // 기본 상태 설정
+                            .build();
+
+                    // 2.3 AudioFile 저장 및 연관 설정
+                    AudioFile originAudio = mapToAudioFile(rowRequest.getOriginAudioRequest(), concatRow);
+                    audioFiles.add(originAudio);
+
+                    return concatRow;
+                }).toList();
+
+        // 3. ConcatRow, 저장
+        concatRowHelper.batchInsert(concatRows);
+        audioFileService.saveAudioFiles(audioFiles);
+        return true;
+    }
+
+    private AudioFile mapToAudioFile(OriginAudioRequest originAudioRequest, ConcatRow concatRow) {
+        // AudioFile 매핑
+        return AudioFile.builder()
+                .concatRow(concatRow)
+                .audioUrl(originAudioRequest.getAudioUrl())
+                .extension(originAudioRequest.getExtension())
+                .fileSize(originAudioRequest.getFileSize())
+                .fileLength(originAudioRequest.getFileLength())
+                .fileName(originAudioRequest.getFileName())
+                .build();
+    }
+
 }
