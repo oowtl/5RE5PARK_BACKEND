75d5802298af9db71a2bd88fe6126d6ca3b54057
K-KY <kyuyoungk@naver.com>
2024-11-23 08:34:55 +0900

    [Feat] : AudioFile S3 저장 요청 Dto

---
+++ src/main/java/com/oreo/finalproject_5re5_be/concat/dto/request/AudioFileRequestDto.java
@@ -0,0 +1,20 @@
+package com.oreo.finalproject_5re5_be.concat.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class AudioFileRequestDto {
+    private MultipartFile audioFile;
+    private String fileName;
+
+    public AudioFileRequestDto(String fileName) {
+        this.fileName = fileName;
+        this.audioFile = null;
+    }
+
+}
