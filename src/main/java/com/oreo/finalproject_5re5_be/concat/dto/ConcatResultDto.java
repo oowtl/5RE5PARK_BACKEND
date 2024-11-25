c2a818a9580b2d461f6d39a7054f205db881f6ff
K-KY <kyuyoungk@naver.com>
2024-11-25 14:17:02 +0900

    [Feat] : ConcatResultDto 추가

---
+++ src/main/java/com/oreo/finalproject_5re5_be/concat/dto/ConcatResultDto.java
@@ -0,0 +1,16 @@
+package com.oreo.finalproject_5re5_be.concat.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ConcatResultDto {
+    private Long concatResultSequence;
+    private Long fileSize;
+    private Float fileLength;
+
+    private String audioUrl;
+    private String fileName;
+    private String extension;
+}
