cec8b7a59fd8d679b60aa0564cde172463a8ba33
K-KY <kyuyoungk@naver.com>
2024-11-25 14:43:38 +0900

    [Feat] : Concat 결과 생성 요청 Dto 추가

---
+++ src/main/java/com/oreo/finalproject_5re5_be/concat/service/AudioMultipartFile.java
@@ -0,0 +1,60 @@
+package com.oreo.finalproject_5re5_be.concat.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+
+public class AudioMultipartFile implements MultipartFile {
+    private final byte[] data;
+    private final String fileName;
+    private final String contentType;
+
+    public AudioMultipartFile(byte[] data, String fileName, String contentType) {
+        this.data = data;
+        this.fileName = fileName;
+        this.contentType = contentType;
+    }
+
+    @Override
+    public String getName() {
+        return fileName;
+    }
+
+    @Override
+    public String getOriginalFilename() {
+        return fileName;
+    }
+
+    @Override
+    public String getContentType() {
+        return contentType;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return data.length == 0;
+    }
+
+    @Override
+    public long getSize() {
+        return data.length;
+    }
+
+    @Override
+    public byte[] getBytes() {
+        return data;
+    }
+
+    @Override
+    public InputStream getInputStream() {
+        return new ByteArrayInputStream(data);
+    }
+
+    @Override
+    public void transferTo(File dest) throws IOException, IllegalStateException {
+        try (FileOutputStream out = new FileOutputStream(dest)) {
+            out.write(data);
+        }
+    }
+
+}
