4b30fdde903e689fd2039f9c3fa4b4d7ce469cfc
oowtl <oowtl7425@gmail.com>
2024-11-29 15:39:31 +0900

    [fix] swaager cors 설정

--- src/main/java/com/oreo/finalproject_5re5_be/FinalProject5Re5BeApplication.java
+++ src/main/java/com/oreo/finalproject_5re5_be/FinalProject5Re5BeApplication.java
@@ -1,5 +1,7 @@
 package com.oreo.finalproject_5re5_be;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
@@ -7,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableRetry // 재시도 복구를 위한 어노테이션
 @EnableScheduling // 스케줄링 처리를 위한 어노테이션
+@OpenAPIDefinition(servers = {@Server(url = "https://5re5park.site", description = "5re5park site")})
 @SpringBootApplication
 public class FinalProject5Re5BeApplication {
 
