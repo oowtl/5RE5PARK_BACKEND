f8ab25c539206c58f3e240e55b0c98c587fda14d
alswooy <108874185+alswooy@users.noreply.github.com>
2024-11-29 18:27:00 +0900

    [fix] swagger servers 지정

--- src/main/java/com/oreo/finalproject_5re5_be/FinalProject5Re5BeApplication.java
+++ src/main/java/com/oreo/finalproject_5re5_be/FinalProject5Re5BeApplication.java
@@ -9,7 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableRetry // 재시도 복구를 위한 어노테이션
 @EnableScheduling // 스케줄링 처리를 위한 어노테이션
-@OpenAPIDefinition(servers = {@Server(url = "https://5re5park.site", description = "5re5park site")})
 @SpringBootApplication
 public class FinalProject5Re5BeApplication {
 
