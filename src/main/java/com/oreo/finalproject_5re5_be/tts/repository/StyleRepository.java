48aa881c5bdcb91372b0ec0f365d2aa9e267e1d2
yunseo <cysdl5604@inu.ac.kr>
2024-12-03 14:31:05 +0900

    [feat]: 언어 식별번호로 목소리가 있는 스타일 목록 조회 메서드 추가

--- src/main/java/com/oreo/finalproject_5re5_be/tts/repository/StyleRepository.java
+++ src/main/java/com/oreo/finalproject_5re5_be/tts/repository/StyleRepository.java
@@ -2,11 +2,18 @@ package com.oreo.finalproject_5re5_be.tts.repository;
 
 import com.oreo.finalproject_5re5_be.tts.entity.Style;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface StyleRepository extends JpaRepository<Style, Long> {
     Optional<Style> findByName(String name);
+
+    // 언어 식별 번호 기반으로 목소리가 있는 스타일을 반환
+    @Query("SELECT DISTINCT v.style FROM Voice v WHERE v.language.langSeq = :langSeq")
+    List<Style> findListBylangSeq(@Param("langSeq") Long langSeq);
 }
