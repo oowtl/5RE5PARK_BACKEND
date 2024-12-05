7ca863524b9a20e2b2dbfe2422c003412b7f3194
oowtl <oowtl7425@gmail.com>
2024-12-06 06:13:27 +0900

    [fix] session management 수정

--- src/main/java/com/oreo/finalproject_5re5_be/member/config/MemberSecurityConfig.java
+++ src/main/java/com/oreo/finalproject_5re5_be/member/config/MemberSecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer.SessionFixationConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -98,7 +100,8 @@ public class MemberSecurityConfig {
                 memberConfig.corsConfigurationSource())) // 새로운 방식으로 CORS 설정 적용
             .csrf(csrf -> csrf.disable()) // CSRF 비활성화
             .sessionManagement(sessionManagement -> sessionManagement.sessionFixation(
-                sessionFixation -> sessionFixation.changeSessionId())) // 기본값
+                    SessionFixationConfigurer::changeSessionId)
+                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)) // 기본값
             .httpBasic(
                 httpSecurityHttpBasicConfigurer -> httpSecurityHttpBasicConfigurer.disable()) // HTTP Basic 인증 비활성화
             .authorizeHttpRequests(authorize -> authorize
