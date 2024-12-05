27f650703fb58cf9b1bdb2a349020691d1d10edc
Ryu-Eun <ektl4m@gmail.com>
2024-12-06 06:36:25 +0900

    [feat] bgmRepo 메서드 추가

--- src/main/java/com/oreo/finalproject_5re5_be/concat/repository/BgmFileRepository.java
+++ src/main/java/com/oreo/finalproject_5re5_be/concat/repository/BgmFileRepository.java
@@ -3,6 +3,7 @@ package com.oreo.finalproject_5re5_be.concat.repository;
 import com.oreo.finalproject_5re5_be.concat.entity.BgmFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,5 +13,9 @@ public interface BgmFileRepository extends JpaRepository<BgmFile, Long> {
     // concatTab seq에 들어가는 bgmFile 정보들 조회
     @Query("SELECT b FROM bgm_file b WHERE b.concatTab.projectId = :tabSeq")
     List<BgmFile> findByConcatTabSeq(Long tabSeq);
+
+    // 특정 ConcatResult에 연결된 모든 BgmFile 조회
+    @Query("SELECT b FROM bgm_file b WHERE b.concatResult.concatResultSequence = :resultSeq")
+    List<BgmFile> findByConcatResultSeq(@Param("resultSeq") Long concatResultSeq);
 
 }
