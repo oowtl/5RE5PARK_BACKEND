6993ca860f4b8aa0fb1eacb92a93405a076cd0b5
Ryu-Eun <ektl4m@gmail.com>
2024-12-06 01:30:33 +0900

    [feat] BgmFile엔티티가 생성되서 따라 생성된 repo,service,controller

---
+++ src/main/java/com/oreo/finalproject_5re5_be/concat/controller/BgmFileController.java
@@ -0,0 +1,27 @@
+package com.oreo.finalproject_5re5_be.concat.controller;
+
+import com.oreo.finalproject_5re5_be.concat.entity.BgmFile;
+import com.oreo.finalproject_5re5_be.concat.service.BgmFileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/bgms")
+public class BgmFileController {
+    private final BgmFileService bgmFileService;
+
+    @GetMapping("/concat-tab/{tabSeq}")
+    public ResponseEntity<List<BgmFile>> getBgmFiles(@PathVariable Long tabSeq) {
+        List<BgmFile> bgmFiles = bgmFileService.getBgmFilesByTabSeq(tabSeq);
+        return ResponseEntity.ok(bgmFiles);
+    }
+
+}
