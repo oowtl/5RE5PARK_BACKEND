7e30588f0862e550ae4bfeb764cf8e294041e3d4
Ryu-Eun <ektl4m@gmail.com>
2024-12-06 06:36:02 +0900

    [feat] bgmController의 responseDto 수정

--- src/main/java/com/oreo/finalproject_5re5_be/concat/dto/ConcatResponseDto.java
+++ src/main/java/com/oreo/finalproject_5re5_be/concat/dto/ConcatResponseDto.java
@@ -1,5 +1,6 @@
 package com.oreo.finalproject_5re5_be.concat.dto;
 
+import com.oreo.finalproject_5re5_be.concat.dto.request.OriginAudioRequest;
 import lombok.*;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ConcatResponseDto {
-    private String audioUrl;
-    private List<RowInfoDto> rows;
+    private String audioUrl; // 결과 파일 URL
+    private OriginAudioRequest bgmFile; // BGM 오디오 파일 정보
+    private List<OriginAudioRequest> concatRowFiles; // ConcatRow에서 사용된 파일 정보 리스트
 }
