edcc7c7df5efd353259b9b73ee2792e389cb64ae
jongheonleee <qwefghnm1212@gmail.com>
2024-12-05 18:40:50 +0900

    [fix] WebConfig 설정 변경하기

--- src/main/java/com/oreo/finalproject_5re5_be/global/config/WebConfig.java
+++ src/main/java/com/oreo/finalproject_5re5_be/global/config/WebConfig.java
@@ -12,10 +12,14 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("https://client.5re5park.site")
                 .allowedOrigins("https://5re5park.site")
                 .allowedOrigins("https://www.5re5park.site")
+                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://5re5park.site:8080")
+                .allowedOrigins("http://127.0.0.1:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .exposedHeaders("location")
                 .allowedHeaders("*")
-                .allowCredentials(false);
+                .allowCredentials(true);
+
         // Swagger를 위한 CORS 설정
         registry.addMapping("/swagger-ui/**")
                 .allowedOrigins("https://client.5re5park.site", "https://5re5park.site", "https://www.5re5park.site")
