fbf265741b86d35d35bbab83a8bde6d177ba1a9c
Ryu-Eun <ektl4m@gmail.com>
2024-12-06 00:22:31 +0900

    [feat] concatTab과 bgmFile 관계 설정

---
+++ src/main/java/com/oreo/finalproject_5re5_be/concat/entity/BgmFile.java
@@ -0,0 +1,40 @@
+package com.oreo.finalproject_5re5_be.concat.entity;
+
+
+import com.oreo.finalproject_5re5_be.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@ToString
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "bgm_file")
+public class BgmFile extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bgm_file_seq")
+    private Long bgmFileSeq;
+
+    @ManyToOne
+    @JoinColumn(name = "concat_tab_seq") // 외래 키: concat_tab의 ID
+    private ConcatTab concatTab;
+
+
+    private String audioUrl;
+    private String extension;
+    private Long fileSize;
+    private Long fileLength;
+    private String fileName;
+    @CreatedDate
+    @Column(updatable = false, name = "created_date")
+    private LocalDateTime createdDate;
+
+
+}
+
