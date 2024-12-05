975e52465ce3bfbda28d38e22e2de2472ca89a73
alswooy <108874185+alswooy@users.noreply.github.com>
2024-12-06 08:13:04 +0900

    [feat] : web conifg 설정

--- src/main/java/com/oreo/finalproject_5re5_be/global/config/WebConfig.java
+++ src/main/java/com/oreo/finalproject_5re5_be/global/config/WebConfig.java
@@ -10,7 +10,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://5re5park.site") // 정확한 도메인 설정
+                .allowedOrigins(
+                        "https://client.5re5park.site",
+                        "https://5re5park.site",
+                        "https://www.5re5park.site",
+                        "http://5re5park.site:8080",
+                        "http://localhost:8080",
+                        "http://localhost:5173",
+                        "http://127.0.0.1:8080",
+                        "http://127.0.0.1:5173") // 정확한 도메인 설정
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true) // 쿠키와 인증 정보 허용
                 .maxAge(3600);
