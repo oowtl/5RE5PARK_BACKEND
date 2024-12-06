4470f33da9618b75f7b232288399cc225ecc775a
Ryu-Eun <ektl4m@gmail.com>
2024-12-06 10:06:11 +0900

    [feat] Dto 추가

---
+++ src/main/java/com/oreo/finalproject_5re5_be/concat/dto/response/ConcatResultResponse.java
@@ -0,0 +1,19 @@
+package com.oreo.finalproject_5re5_be.concat.dto.response;
+
+import com.oreo.finalproject_5re5_be.concat.dto.request.OriginAudioRequest;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Builder
+@Getter
+public class ConcatResultResponse {
+    private Long concatResultSeq;
+    private String audioUrl;
+    private String extension;
+    private Float fileLength;
+    private String fileName;
+    private OriginAudioRequest bgmFile; // 사용된 BGM 파일
+    private List<OriginAudioRequest> materialAudioFiles; // 재료 오디오 파일 목록
+}
