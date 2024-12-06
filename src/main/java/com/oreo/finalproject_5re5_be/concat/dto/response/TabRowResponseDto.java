7e4faf5a22d5577597aaa12dfbccaddc2c067d96
K-KY <kyuyoungk@naver.com>
2024-12-06 13:42:47 +0900

    [Feat] : 탭, 행 조회시 사용할 DTO

---
+++ src/main/java/com/oreo/finalproject_5re5_be/concat/dto/response/TabRowResponseDto.java
@@ -0,0 +1,15 @@
+package com.oreo.finalproject_5re5_be.concat.dto.response;
+
+import com.oreo.finalproject_5re5_be.concat.dto.request.ConcatRowSaveRequestDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class TabRowResponseDto {
+    private ConcatTabResponseDto concatTab;
+    private ConcatRowSaveRequestDto concatRows;
+}
