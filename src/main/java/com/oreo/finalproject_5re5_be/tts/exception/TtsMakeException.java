f5c7a2299ad4e397c450ee3ae87e7a5df38f7318
yunseo <cysdl5604@inu.ac.kr>
2024-12-06 11:54:49 +0900

    [feat]: tts 생성 예외 추가

---
+++ src/main/java/com/oreo/finalproject_5re5_be/tts/exception/TtsMakeException.java
@@ -0,0 +1,15 @@
+package com.oreo.finalproject_5re5_be.tts.exception;
+
+import com.oreo.finalproject_5re5_be.global.exception.BusinessException;
+import com.oreo.finalproject_5re5_be.global.exception.ErrorCode;
+
+public class TtsMakeException extends BusinessException {
+
+    public TtsMakeException() {
+        super(ErrorCode.INTERNAL_SERVER_ERROR.getMessage(), ErrorCode.INTERNAL_SERVER_ERROR);
+    }
+
+    public TtsMakeException(String message) {
+        super(message, ErrorCode.INTERNAL_SERVER_ERROR);
+    }
+}
