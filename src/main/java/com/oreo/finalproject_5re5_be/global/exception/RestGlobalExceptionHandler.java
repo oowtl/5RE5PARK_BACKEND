bc3a2f2370c6ca6c9e91439375ca1e53f4aa9bcd
K-KY <kyuyoungk@naver.com>
2024-12-10 01:45:21 +0900

    [Feat] : 글로벌 예외 핸들러 예외 추가

--- src/main/java/com/oreo/finalproject_5re5_be/global/exception/RestGlobalExceptionHandler.java
+++ src/main/java/com/oreo/finalproject_5re5_be/global/exception/RestGlobalExceptionHandler.java
@@ -5,11 +5,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MultipartException;
 
 @RestControllerAdvice
 public class RestGlobalExceptionHandler {
     @ExceptionHandler(DataNotFoundException.class)
     public ResponseEntity<ResponseDto<String>> handleDataNotFoundException(DataNotFoundException ex) {
         return new ResponseDto<>(HttpStatus.NOT_FOUND.value(), ex.getMessage()).toResponseEntity();
+    }
+
+
+    @ExceptionHandler(MultipartException.class)
+    public ResponseEntity<ResponseDto<String>> handleMultipartException(MultipartException ex) {
+        return new ResponseDto<>(HttpStatus.BAD_REQUEST.value(), ex.getMessage()).toResponseEntity();
     }
 }
