811e484ce2b9b88d4d151f9542c49154acb9d82f
K-KY <kyuyoungk@naver.com>
2024-12-12 14:38:24 +0900

    [Feat] : 컨트롤러가 람다를 호출하도록 변경

--- src/main/java/com/oreo/finalproject_5re5_be/concat/controller/ConcatController.java
+++ src/main/java/com/oreo/finalproject_5re5_be/concat/controller/ConcatController.java
@@ -5,8 +5,8 @@ import com.oreo.finalproject_5re5_be.concat.dto.ConcatResultDto;
 import com.oreo.finalproject_5re5_be.concat.dto.request.ConcatRowRequestDto;
 import com.oreo.finalproject_5re5_be.concat.dto.response.ConcatTabResponseDto;
 import com.oreo.finalproject_5re5_be.concat.service.ConcatResultService;
-import com.oreo.finalproject_5re5_be.concat.service.ConcatService;
 import com.oreo.finalproject_5re5_be.concat.service.ConcatTabService;
+import com.oreo.finalproject_5re5_be.concat.service.lambda.LambdaConcatService;
 import com.oreo.finalproject_5re5_be.global.dto.response.ResponseDto;
 import com.oreo.finalproject_5re5_be.member.dto.CustomUserDetails;
 import com.oreo.finalproject_5re5_be.project.service.ProjectService;
@@ -21,13 +21,14 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.concurrent.ExecutionException;
 
 @Tag(name = "Concat", description = "Concat 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/concat")
 public class ConcatController {
-    private final ConcatService concatService;
+    private final LambdaConcatService lambdaConcatService;
     private final ConcatTabService concatTabService;
     private final ConcatResultService concatResultService;
     private final ProjectService projectService;
@@ -37,15 +38,30 @@ public class ConcatController {
             description = "병합이 성공 했다면 저장된 오디오 정보를 반환합니다."
     )
     @PostMapping("")
-    public ResponseEntity<ResponseDto<ConcatResultDto>> concat(@RequestBody ConcatRowRequestDto audioRequests,
-                                                               @AuthenticationPrincipal CustomUserDetails customUserDetails) throws IOException {
+    public ResponseEntity<ResponseDto<List<ConcatResultDto>>> concat(@RequestBody ConcatRowRequestDto audioRequests,
+                                                                     @AuthenticationPrincipal CustomUserDetails customUserDetails) throws IOException, ExecutionException, InterruptedException {
         projectService.projectCheck(customUserDetails.getMember().getSeq(), audioRequests.getConcatTabId());
 
         ConcatTabResponseDto concatTabResponseDto
                 = concatTabService.readConcatTab(audioRequests.getConcatTabId(), customUserDetails.getMember().getSeq());
-        ConcatResultDto concat = concatService.concat(concatTabResponseDto, audioRequests);
-        return new ResponseDto<>(HttpStatus.OK.value(), concat).toResponseEntity();
+
+        List<ConcatResultDto> concatResult = lambdaConcatService.concatOnLambda(concatTabResponseDto, audioRequests);
+
+        return new ResponseDto<>(HttpStatus.OK.value(), concatResult).toResponseEntity();
     }
+
+//    @PostMapping("")
+//    public ResponseEntity<ResponseDto<ConcatResultDto>> concat(@RequestBody ConcatRowRequestDto audioRequests,
+//                                                               @AuthenticationPrincipal CustomUserDetails customUserDetails) throws IOException {
+//        projectService.projectCheck(customUserDetails.getMember().getSeq(), audioRequests.getConcatTabId());
+//
+//        ConcatTabResponseDto concatTabResponseDto
+//                = concatTabService.readConcatTab(audioRequests.getConcatTabId(), customUserDetails.getMember().getSeq());
+//        ConcatResultDto concat = concatService.concat(concatTabResponseDto, audioRequests);
+//        return new ResponseDto<>(HttpStatus.OK.value(), concat).toResponseEntity();
+//    }
+
+
 
     @Operation(
             summary = "오디오 결과 목록 불러오기",
