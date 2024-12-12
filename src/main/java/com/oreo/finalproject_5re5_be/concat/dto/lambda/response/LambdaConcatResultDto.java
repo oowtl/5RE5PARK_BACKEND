59023b44999926849828bdaf75d95a0842254ceb
K-KY <kyuyoungk@naver.com>
2024-12-12 14:34:29 +0900

    [Feat] : 람다 호출용 DTO

---
+++ src/main/java/com/oreo/finalproject_5re5_be/concat/dto/lambda/response/LambdaConcatResultDto.java
@@ -0,0 +1,48 @@
+package com.oreo.finalproject_5re5_be.concat.dto.lambda.response;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.oreo.finalproject_5re5_be.concat.dto.request.OriginAudioRequest;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class LambdaConcatResultDto {
+    private final AudioInfo info;
+    private final List<OriginAudioRequest> processed;
+    private final String s3Url;
+    private final String contentType;
+    private final String processId;
+    private final int i;
+
+    @JsonCreator
+    public LambdaConcatResultDto(
+            @JsonProperty("info") AudioInfo info,
+            @JsonProperty("processed") List<OriginAudioRequest> processed,
+            @JsonProperty("s3Url") String s3Url,
+            @JsonProperty("contentType") String contentType,
+            @JsonProperty("processId") String processId,
+            @JsonProperty("i") int i
+    ) {
+        this.info = info;
+        this.processed = processed;
+        this.s3Url = s3Url;
+        this.contentType = contentType;
+        this.processId = processId;
+        this.i = i;
+    }
+
+    @Override
+    public String toString() {
+        return "LambdaConcatResultDto{" +
+                ", processed=" + processed +
+                ", s3Url='" + s3Url + '\'' +
+                ", contentType='" + contentType + '\'' +
+                ", processId='" + processId + '\'' +
+                ", i=" + i +
+                '}';
+    }
+}
